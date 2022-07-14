7c7d2fe2fb05b4fc4d9f418d46d50fb0c24b79dd
hotequil <hotequil@protonmail.com>
2022-07-13 21:48:56 -0300

    feat: created routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { Photos } from "./pages/Photos";
+import { Photo } from "./pages/Photo";
+import { Route, Routes, BrowserRouter } from "react-router-dom"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route path="/" element={<Photos />}/>
+          <Route path="/:photoId" element={<Photo />}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
