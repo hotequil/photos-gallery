69fc2066ea1058de015b0bec7ea61e0f77cee29f
hotequil <hotequil@protonmail.com>
2022-07-13 21:33:09 -0300

    feat: init Photo and Photos

---
+++ src/pages/Photo.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Photo: React.FC = () => {
+  return <h1>Hello photo</h1>
+}
---
+++ src/pages/Photos.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Photos: React.FC = () => {
+  return <h1>Hello photos</h1>
+}
