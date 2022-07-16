489b49d248702ce23760a50c11a8bbe3197f6b92
hotequil <hotequil@protonmail.com>
2022-07-16 15:19:52 -0300

    feat: validate internal and external link

--- src/components/link/Link.tsx
+++ src/components/link/Link.tsx
@@ -1,6 +1,7 @@
 import React, { PropsWithChildren } from "react";
 import styles from "./Link.module.css"
 import PropTypes from "prop-types"
+import { Link as LinkElement } from "react-router-dom"
 
 export enum Target{
   BLANK = "_blank",
@@ -12,9 +13,10 @@ interface LinkProps{
   target?: Target
 }
 
-export const Link: React.FC<PropsWithChildren<LinkProps>> = ({ href, target, children }) => {
-  return <a className={styles.link} href={href} target={target}>{children}</a>
-}
+export const Link: React.FC<PropsWithChildren<LinkProps>> = ({ href, target, children }) =>
+  href.startsWith("http") ?
+  <a href={href} className={styles.link} target={target}>{children}</a> :
+  <LinkElement to={href} className={styles.link} target={target}>{children}</LinkElement>
 
 Link.propTypes = {
   href: PropTypes.string.isRequired,
