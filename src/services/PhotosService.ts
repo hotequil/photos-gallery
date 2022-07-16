1a7bc10d324f06543e8a227455057400a3779254
hotequil <hotequil@protonmail.com>
2022-07-15 21:25:29 -0300

    feat: created PhotosService

---
+++ src/services/PhotosService.ts
@@ -0,0 +1,47 @@
+export interface Photo{
+  albumId: number,
+  id: number,
+  title: string,
+  url: string,
+  thumbnailUrl: string
+}
+
+export type Photos = Photo[]
+
+export class PhotosService{
+  private url = import.meta.env.VITE_API_URL
+
+  getList(): Promise<Photos>{
+    return new Promise<Photos>(resolve => {
+      setTimeout(async () => {
+        try {
+          const response = await fetch(this.url)
+          const list = await response.json()
+
+          resolve(list.splice(0, 20))
+        } catch(error){
+          console.error(error)
+
+          resolve([])
+        }
+      }, 2000)
+    })
+  }
+
+  getItem(id: number): Promise<Photo|null>{
+    return new Promise<Photo|null>(resolve => {
+      setTimeout(async () => {
+        try {
+          const response = await fetch(`${this.url}/${id}`)
+          const item = await response.json()
+
+          resolve(item)
+        } catch(error){
+          console.error(error)
+
+          resolve(null)
+        }
+      }, 1000)
+    })
+  }
+}
